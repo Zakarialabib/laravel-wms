e21166758e98cdb2a7544fcc23bebdf52a93dc24
ismaail <contact@ismaail.com>
2022-11-05 02:32:03 +0100

    FIX broken Delivery Livewire

--- app/Http/Livewire/Delivery.php
+++ app/Http/Livewire/Delivery.php
@@ -2,12 +2,11 @@
 
 namespace App\Http\Livewire;
 
-use Livewire\Component;
-use App\Models\Deliveries;
-use App\Models\Products;
 use App\Models\User;
 use App\Models\Sale;
-use Illuminate\Http\Request;
+use Livewire\Component;
+use App\Models\Products;
+use App\Models\Deliveries;
 use Livewire\WithPagination;
 use Illuminate\Support\Facades\Auth;
 
@@ -15,7 +14,21 @@ class Delivery extends Component
 {
     use WithPagination;
 
-    public $delivery_id, $search, $sale_id, $user_id, $tracking_number, $recipient, $address, $expected_arrival, $actual_arrival, $status, $description, $price, $deleteId;
+    private $deliveries;
+    public $delivery_id;
+    public $search;
+    public $sale_id;
+    public $user_id;
+    public $tracking_number;
+    public $recipient;
+    public $address;
+    public $expected_arrival;
+    public $actual_arrival;
+    public $status;
+    public $description;
+    public $price;
+    public $deleteId;
+
     protected $queryString = ['search'];
 
     public function mount(): void
@@ -25,78 +38,70 @@ class Delivery extends Component
 
     public function render()
     {
-     ///   $this->sales = Deliveries::query()
-     /// ->where('user_id', Auth::id());
-     
-     $products = Products::all();
-     $users = User::all();
-     $sales = Sale::all();
+        $products = Products::all();
+        $users = User::all();
+        $sales = Sale::all();
 
-     $this->deliveries = Deliveries::paginate(5);
-        return view('livewire.delivery', compact('sales','users','products'),[
-            'deliveries' => $this->search === null ?
-            Deliveries::paginate(5) :
-            Deliveries::where('status', 'like', '%' . $this->search . '%')
-                ->orderBy('created_at', 'desc')->paginate(5)
+        $this->deliveries = Deliveries::paginate(5);
+
+        return view('livewire.delivery', compact('sales', 'users', 'products'), [
+            'deliveries' => $this->search === null
+                ? Deliveries::paginate(5)
+                : Deliveries::where('status', 'like', '%' . $this->search . '%')
+                    ->orderBy('created_at', 'desc')
+                    ->paginate(5),
         ]);
     }
 
     public function store()
     {
         if (Sale::where('id')) {
-        $this->validate([
-            'sale_id' => 'required|unique:deliveries',
-            'tracking_number' => 'required',
-            'recipient' => 'required',
-            'address' => 'required',
-            'price' => 'required',
-            'expected_arrival' => 'required|date',
-            'actual_arrival' => 'nullable|date|after:expected_arrival',
-            'status' => 'required',
-            'description' => 'required',
-            'user_id' => ''
-        ]);
-        Deliveries::updateOrCreate(['id' => $this->delivery_id], [
-            'sale_id' => $this->sale_id,
-            'tracking_number' => $this->tracking_number,
-            'recipient' => $this->recipient,
-            'address' => $this->address,
-            'price' => $this->price,
-            'expected_arrival' =>$this->expected_arrival,
-            'actual_arrival' => $this->actual_arrival,
-            'status' => $this->status,
-            'description' => $this->description, 
-            'user_id' => $this->user_id  = Auth::id(), 
-        ]);
-        return  session()->flash('message', 
-            $this->delivery_id ? 'Deliveries Created Successfully' : 'Product Created Successfully.');
-        } else {
-            return  back()->with('error', 'Deliveries ID doesn\'t exist!');
+            $this->validate([
+                'sale_id' => 'required|unique:deliveries',
+                'tracking_number' => 'required',
+                'recipient' => 'required',
+                'address' => 'required',
+                'price' => 'required',
+                'expected_arrival' => 'required|date',
+                'actual_arrival' => 'nullable|date|after:expected_arrival',
+                'status' => 'required',
+                'description' => 'required',
+                'user_id' => ''
+            ]);
+
+            Deliveries::updateOrCreate(['id' => $this->delivery_id], [
+                'sale_id' => $this->sale_id,
+                'tracking_number' => $this->tracking_number,
+                'recipient' => $this->recipient,
+                'address' => $this->address,
+                'price' => $this->price,
+                'expected_arrival' => $this->expected_arrival,
+                'actual_arrival' => $this->actual_arrival,
+                'status' => $this->status,
+                'description' => $this->description,
+                'user_id' => $this->user_id  = Auth::id(),
+            ]);
+
+            session()->flash(
+                'message',
+                $this->delivery_id ? 'Deliveries Created Successfully' : 'Product Created Successfully.'
+            );
+
+            return response();
         }
+
+        return  back()->with('error', 'Deliveries ID doesn\'t exist!');
     }
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @return response()
-     */
     public function deleteId($id)
-
     {
         $this->deleteId = $id;
         session()->flash('message', 'Delivery Deleted Successfully.');
-
     }
 
-      /**
-     * The attributes that are mass assignable.
-     *
-     * @return response()
-     */
     public function delete()
     {
         Deliveries::find($this->deleteId)->delete();
         session()->flash('message', 'Delivery Deleted Successfully.');
-
     }
 }
